9d499e9be213c84b90845031144ae6f4d9f66cca
Rod <rodfox.developers@gmail.com>
2023-04-29 15:52:09 +0200

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import BannerHome from './components/home/BannerHome';
-import DescripcionHome from './components/home/DescripcionHome';
-import IntegrantesHome from './components/home/IntegrantesHome';
-import VideoHome from './components/home/VideoHome';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import InicioPage from './pages/InicioPage';
+import DiscografiaPage, { loader as spotifyLoader } from './pages/DiscografiaPage';
+import VideosPage from './pages/VideosPage';
+import BiografiaPage from './pages/BiografiaPage';
+import EventosPage from './pages/EventosPage';
+import ContactoPage from './pages/ContactoPage';
+import RiderPage from './pages/RiderPage';
+import CachePage from './pages/CachePage';
+import RootLayout from './pages/RootLayout';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  { path:'/', 
+  element: <RootLayout />,
+  errorElement: <ErrorPage />,
+  children: [
+    { path:'/', element: <InicioPage /> },
+    { path:'/discografia', element: <DiscografiaPage />, loader: spotifyLoader },
+    { path:'/videos', element: <VideosPage /> },
+    { path:'/biografia', element: <BiografiaPage /> },
+    { path:'/eventos', element: <EventosPage /> },
+    { path:'/contacto', element: <ContactoPage /> },
+    { path:'/rider', element: <RiderPage /> },
+    { path:'/cache', element: <CachePage /> },
+  ]
+  }
+])
 
 function App() {
   return (
-    <div>
-      <BannerHome />
-      <DescripcionHome />
-      <IntegrantesHome />
-      <VideoHome />
-    </div>
+    <div className="container-fluid">      
+      <RouterProvider router={router} />
+    </div>    
   );
 }
 
