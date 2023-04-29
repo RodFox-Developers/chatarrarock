398fa4cf31e6fd334ca729611beb5d1b934742d3
Rod <rodfox.developers@gmail.com>
2023-04-29 15:53:20 +0200

    home components

---
+++ src/components/home/MediaHome.js
@@ -0,0 +1,28 @@
+function MediaHome() {
+  return (
+    <div className="row bg-light py-5 my-5">
+        <div className="col-12 text-center text-info">
+            <h3 className="align-middle">¡¡Escuchanos en!!</h3>  
+        </div>
+        <div className="col-12 social-logos text-center">
+            <a href="https://www.youtube.com/channel/UCSrQ7KZ_VdEGsE53ALl-Z-g" target="_blank" rel="noreferrer">
+                <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-c89b9.appspot.com/o/images%2FYouTube_Logo.svg.png?alt=media&token=0f4e7ad5-f8a2-4b62-b7ae-073c7615ba06" className="img-fluid mx-5" alt="youtube logo"></img>
+            </a>  
+            <a href="https://open.spotify.com/artist/7AtmpUfNKk9jraaAoOv9Da" target="_blank" rel="noreferrer">
+                <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-c89b9.appspot.com/o/images%2Fspotify.png?alt=media&token=4066f0dd-3ef8-4c08-b00d-b2db0b212df5" className="img-fluid mx-5 mt-5 mt-sm-0" alt="spotify logo"></img>
+            </a> 
+            <a href="https://soundcloud.com/chatarra-rock" target="_blank" rel="noreferrer">
+                <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-c89b9.appspot.com/o/images%2FSoundcloud.png?alt=media&token=d118ea0f-ef99-42ad-a099-a9abf7188262" className="img-fluid mx-5 mt-5 mt-sm-0" alt="Soundcloud logo"></img>
+            </a> 
+            <a href="https://www.jamendo.com/artist/342136/chatarra" target="_blank" rel="noreferrer">
+                <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-c89b9.appspot.com/o/images%2Fjamendo-logo.svg?alt=media&token=3706f919-2482-45d4-9594-bf9f0940c8fd" className="img-fluid mx-5 mt-5 mt-sm-0" alt="jamendo logo"></img>
+            </a> 
+            <a href="https://chatarra.bandcamp.com/" target="_blank" rel="noreferrer">
+                <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-c89b9.appspot.com/o/images%2Fbandcamp-logo.png?alt=media&token=759d2978-4c3f-445b-bacc-03127628a3fb" className="img-fluid mx-5 mt-5 mt-sm-0" alt="bandcamp logo"></img>
+            </a> 
+        </div>
+    </div>
+  )
+}
+
+export default MediaHome;
--- src/components/home/VideoHome.js
+++ src/components/home/VideoHome.js
@@ -1,9 +1,9 @@
 function VideoHome() {
   return (
-    <div class="row my-5 justify-content-center">
-        <div class="col-lg-6">
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" width="100%" height="450" src="https://www.youtube.com/embed/Wk7JTPMrFLc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen  title="Chatarra Youtube"></iframe>   
+    <div className="row my-5 justify-content-center">
+        <div className="col-lg-6">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" width="100%" height="450" src="https://www.youtube.com/embed/Wk7JTPMrFLc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen  title="Chatarra Youtube"></iframe>   
             </div>         
         </div>
     </div> 
