73002346cbf4738eafd0210251c1b42d2f71d0d1
Rod <rodfox.developers@gmail.com>
2024-03-18 20:46:48 +0100

    home enlaces appurl

--- src/components/home/MediaHome.js
+++ src/components/home/MediaHome.js
@@ -5,19 +5,16 @@ function MediaHome() {
             <h3 className="align-middle">¡¡Escuchanos en!!</h3>  
         </div>
         <div className="col-12 social-logos text-center">
-            <a href="https://www.youtube.com/channel/UCSrQ7KZ_VdEGsE53ALl-Z-g" target="_blank" rel="noreferrer">
+            <a href="https://appurl.io/3r7LfdDgjo" target="_blank" rel="noreferrer">
                 <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-rock.appspot.com/o/images%2Fyoutube.png?alt=media&token=eec3c9ba-8213-4526-a648-d135d1105614" className="img-fluid mx-5" alt="youtube logo"></img>
             </a>  
-            <a href="https://open.spotify.com/artist/7AtmpUfNKk9jraaAoOv9Da" target="_blank" rel="noreferrer">
+            <a href="https://appurl.io/TzLcAIS-kx" target="_blank" rel="noreferrer">
                 <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-rock.appspot.com/o/images%2Fspotify.png?alt=media&token=919631ae-7134-432f-8234-7b079f58ce07" className="img-fluid mx-5 mt-5 mt-sm-0" alt="spotify logo"></img>
             </a> 
-            <a href="https://soundcloud.com/chatarra-rock" target="_blank" rel="noreferrer">
+            <a href="https://appurl.io/KSHH6WkWci" target="_blank" rel="noreferrer">
                 <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-rock.appspot.com/o/images%2Fsoundcloud.png?alt=media&token=78244392-93d0-43c9-b9f9-c45216e201e0" className="img-fluid mx-5 mt-5 mt-sm-0" alt="Soundcloud logo"></img>
             </a> 
-            <a href="https://www.jamendo.com/artist/342136/chatarra" target="_blank" rel="noreferrer">
-                <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-rock.appspot.com/o/images%2Fjamendo.png?alt=media&token=92ee56eb-578f-4753-831a-2b88dc7f06f8" className="img-fluid mx-5 mt-5 mt-sm-0" alt="jamendo logo"></img>
-            </a> 
-            <a href="https://chatarra.bandcamp.com/" target="_blank" rel="noreferrer">
+            <a href="https://appurl.io/x1MbsXtvao" target="_blank" rel="noreferrer">
                 <img src="https://firebasestorage.googleapis.com/v0/b/chatarra-rock.appspot.com/o/images%2Fbandcamp.png?alt=media&token=8adddec7-1832-4d7a-abca-6f6bda54f308" className="img-fluid mx-5 mt-5 mt-sm-0" alt="bandcamp logo"></img>
             </a> 
         </div>
